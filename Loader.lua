10f7c24b2dfa2ae220c2b543b3b30942b6ee883b
Jamster707 <78559406+Jamster707@users.noreply.github.com>
2022-09-14 15:50:27 +0100

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,7 +1,7 @@
-loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Jamster707/main.lua"))()
+loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Jamster707/PL2/main/main.lua"))()
 LocalPlayer.OnTeleport:Connect(function(State)
     if State == Enum.TeleportState.Started then
         local QueueOnTeleport = (syn and syn.queue_on_teleport) or queue_on_teleport
-        QueueOnTeleport(loadstring(game:HttpGetAsync("https://raw.githubusercontent.com"))())
+        QueueOnTeleport(loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Jamster707/PL2/main/main.lua"))())
     end
 end)
